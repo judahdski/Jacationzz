285fcc3088893ee19a6908a27ad28afd09655705
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-13 21:55:41 +0700

    feat: tambah layar get started
    nambahin background, tulisan selamat datang, button

--- app/src/main/java/com/d3if0002/jacationzz/StartActivity.kt
+++ app/src/main/java/com/d3if0002/jacationzz/StartActivity.kt
@@ -1,11 +1,24 @@
 package com.d3if0002.jacationzz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class StartActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val startButton = findViewById<Button>(R.id.start_btn)
+        startButton.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.start_btn) {
+            val intent = Intent(this@StartActivity, MainActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
