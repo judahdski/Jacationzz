a78140e01f99301ef9bc6051f1fbc4cf516a321c
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-13 20:14:03 +0700

    feat: buat halaman profile
    buat halaman profile, isinya foto sementara, nama, email

---
+++ app/src/main/java/com/d3if0002/jacationzz/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.d3if0002.jacationzz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
