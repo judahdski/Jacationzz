c83a4aa5c671dd1e7c78b3eccff23ee629cb496e
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-14 11:53:20 +0700

    test: testing function getPlace()
    ngecompare return value dari getPlace() sama hasil yg diharapkan

--- app/src/test/java/com/d3if0002/jacationzz/data/PlacesDataTest.kt
+++ app/src/test/java/com/d3if0002/jacationzz/data/PlacesDataTest.kt
@@ -1,5 +1,6 @@
 package com.d3if0002.jacationzz.data
 
+import com.d3if0002.jacationzz.model.Place
 import org.junit.Assert.*
 
 import org.junit.Test
@@ -8,5 +9,18 @@ class PlacesDataTest {
 
     @Test
     fun getPlace() {
+        val expectResult = Place(
+            id = 0L,
+            thumbnailPic = "https://c1.wallpaperflare.com/preview/489/771/631/eiffel-tower-paris-france-night.jpg",
+            heroPic = "https://images.wallpapersden.com/image/download/eiffel-tower-paris-beautiful-view_amxuZmaUmZqaraWkpJRobWllrWdpZWU.jpg",
+            name = "Eiffel Tower",
+            location = "Paris, Paris",
+            rating = 4.8,
+            description = "The symbol of Paris and one of the most photographed structures in the world, a visit to the Eiffel Tower is a must for all travelers. Few landmarks inspire such a passion for travel as this single iron structure.\nYoung travelers heading out on the road for the first time, couples looking for a special getaway, artists looking to spur their creativity, and romantics of all types are all drawn to Paris. This is a city where history and culture collide and where travelers of all kinds can find the experience they're after."
+        )
+
+        val actualResult = PlacesData.getPlace(0L)
+
+        assertEquals(expectResult, actualResult)
     }
 }
