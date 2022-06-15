77b2d13a188a76bf02096a92716a08d63ce119f8
Judah_Joshua_Martin_Dasuki <judahdasuki@student.telkomuniversity.ac.id>
2022-06-15 14:58:38 +0700

    fix: update deprecated code
    update deprecated code di splash screen, ubah pengaturan memori untuk nampilin gambar bitmap, hapus beberapa unused res file

--- app/src/main/java/com/d3if0002/jacationzz/SplashActivity.kt
+++ app/src/main/java/com/d3if0002/jacationzz/SplashActivity.kt
@@ -1,20 +1,21 @@
 package com.d3if0002.jacationzz
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Looper
 
-
-class Splash : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed(Runnable {
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 1500)
     }
 }
